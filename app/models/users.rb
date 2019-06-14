4dee8eb2b12bafca5214e975d9fc3d459fa14222
Thomas Pope <tpope0928@gmail.com>
2019-06-14 01:36:07 +0000

    added user migration and user class

---
+++ app/models/users.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  has_many :crossfit_workouts
+
+
+end
